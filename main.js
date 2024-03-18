fee325422a5dbd7e844d3d736fda125e8dc46bf7
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-03-18 03:09:58 -0700

    I want to learn so I add this commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt("String typedagi malumotni kiriting");
+let number = prompt("Number typedagi malumotni kiriting");
+let numberFrost = prompt("Buling typedagi malumotni kiriting");
+alert("Hamma malumotlarni tog'ri kiritganingizga ishonchingiz komilmi?");
+alert("Siz kiritgan Zubayr string typega tegishli \n Siz kiritgan 123 soni number tyepiga tegishli \n Siz kiritgan true sozi boolean typega tegishli");
